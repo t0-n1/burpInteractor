202596fa5896c60fc0e3cb96a014d428e9a79085
t0-n1 <52384994+t0-n1@users.noreply.github.com>
2023-01-11 20:36:57 +0100

    Hide progress bar

--- bi-client.ps1
+++ bi-client.ps1
@@ -1,3 +1,5 @@
+$ProgressPreference = "SilentlyContinue"
+
 $HEADERS = @{
     "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
     "Accept-Encoding" = "gzip, deflate"
